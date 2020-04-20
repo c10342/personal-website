be298c7e6956538d53500a419ac21187bf9f8a67
c10342 <you@example.com>
2020-04-20 22:05:54 +0800

    feat：添加七牛云文件上传

---
+++ reception/upload/config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+module.exports = {
+    accessKey: 'YBEYWaqwXhGNpNIuEOsuSyLtQ0i0b34gobjSYsKL',
+    secretKey: '3BCpdcLZZ3wXw-l5psmu-D8RHjnmmlJnciUTeVuK',
+    bucket: "per-website",
+    basePath :path.join(__dirname,'../build') //打包目录
+};
